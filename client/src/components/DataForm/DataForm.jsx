3f715ba88f74cbe98819fb1d149ec63f87756ee6
Sam Sweigart <samuel.sweigart@gmail.com>
2022-05-22 19:07:51 -0400

    added state dropdown menu

--- client/src/components/DataForm/DataForm.jsx
+++ client/src/components/DataForm/DataForm.jsx
@@ -2,87 +2,112 @@ import './DataForm.css';
 import { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_BOULDER } from '../../utils/mutations';
+const UsaStates = require('usa-states').UsaStates;
 
 const DataForm = () => {
 
-    const [ boulderState, setBoulderState ] = useState(
-        {
-            state: '',
-            destination: '',
-            area: '',
-            sub_area: '',
-            boulder_name: '',
-            grade: '',
-            stars: '',
-            coords: '',
-        }
-    );
+    const usStates = new UsaStates();
 
-    const [ createBoulder, { error } ] = useMutation(ADD_BOULDER);
+    const [boulderState, setBoulderState] = useState({
+        state: '',
+        destination: '',
+        area: '',
+        sub_area: '',
+        boulder_name: '',
+        grade: '',
+        stars: '',
+        coords: '',
+    });
+
+    const statesAbbrArr = Object.entries(usStates.states).map(([ key, value ]) => {
+        return { label: value.abbreviation , value: key }
+    });
+   
+    const [createBoulder, { error }] = useMutation(ADD_BOULDER);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setBoulderState({ ...boulderState, [name]: value });
+        return setBoulderState({ ...boulderState, [name]: value });
     };
 
     const addBoulder = async (e) => {
         e.preventDefault();
-        
+
         await createBoulder({
-            variables: { ...boulderState }
+            variables: { ...boulderState },
         });
-        
-        //console.log('BoulderState:', boulderState)
-        
+
+        console.log('BoulderState:', boulderState)
+
         if (error) throw error;
 
         //! reset form
-        setBoulderState({ 
-            state: '',
-            destination: '',
-            area: '',
-            sub_area: '',
-            boulder_name: '',
-            grade: '',
-            stars: '',
-            coords: '',
-         });
+        setBoulderState({
+        state: '',
+        destination: '',
+        area: '',
+        sub_area: '',
+        boulder_name: '',
+        grade: '',
+        stars: '',
+        coords: '',
+        });
     };
 
-    return ( 
+    return (
+
         <section>
-            <h2> Boulder Information </h2>
+        <h2> Boulder Information </h2>
             <form onSubmit={addBoulder}>
-                <input
-                    name='state'
-                    type='text'
-                    placeholder='State'
-                    value={ boulderState.state}
-                    onChange={handleChange}/>
+                <select 
+                    className='select' 
+                    name='state' 
+                    value={boulderState.state} 
+                    onChange={handleChange}>
+                    <option value='none' > Select State </option>
+
+                    { statesAbbrArr.map(({ label, value}) => {
+                        return (
+                            <option 
+                                name='state' 
+                                type='text' 
+                                key={value} 
+                                value={label} 
+                                onChange={handleChange}
+                            > 
+                                {label}
+                            </option>
+                        )}
+                    )}
+                    ∂
+                </select>
+
                 <input
                     name='destination'
                     type='text'
                     placeholder='Destination'
-                    value={ boulderState.destination } 
-                    onChange={handleChange}/>
+                    value={boulderState.destination}
+                    onChange={handleChange}
+                />
                 <input
                     name='area'
                     type='text'
                     placeholder='Area'
-                    value={ boulderState.area }
+                    value={boulderState.area}
                     onChange={handleChange}
                 />
                 <input
                     name='sub_area'
                     type='text'
                     placeholder='Sub Area'
-                    value={ boulderState.sub_area }
-                    onChange={handleChange}/>
+                    value={boulderState.sub_area}
+                    onChange={handleChange}
+                />
                 <input
                     name='boulder_name'
                     type='text'
                     placeholder='Boulder Name'
-                    value={ boulderState.boulder_name }
+                    value={boulderState.boulder_name}
                     onChange={handleChange}
                     required
                 />
@@ -90,27 +115,29 @@ const DataForm = () => {
                     name='grade'
                     type='number'
                     placeholder='Grade'
-                    value={ boulderState.grade }
-                    onChange={handleChange}/>
+                    value={boulderState.grade}
+                    onChange={handleChange}
+                />
                 <input
                     name='stars'
                     type='number'
                     placeholder='Number of Stars'
-                    value={ boulderState.stars }
+                    value={boulderState.stars}
                     onChange={handleChange}
                 />
                 <input
                     name='coords'
                     type='text'
                     placeholder='Coordinates'
-                    value={ boulderState.coords }
+                    value={boulderState.coords}
                     onChange={handleChange}
                 />
-                <input type="file" />
+                <input type='file' />
                 <button type='submit'> Submit </button>
             </form>
         </section>
-     );
+        
+    );
 };
- 
+
 export default DataForm;
