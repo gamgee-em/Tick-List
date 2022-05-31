768484c6e41bc568134b78003a1d9afeabb40ace
Sam Sweigart <samuel.sweigart@gmail.com>
2022-05-31 17:48:53 -0400

    added conditional to ticklist to render default UI for when data does not exist

--- client/src/components/TickList/TickList.jsx
+++ client/src/components/TickList/TickList.jsx
@@ -2,28 +2,30 @@ import './TickList.css';
 
 const TickList = ({ user }) => {
 
-    if (!user.ticks.length) {
-        return <h3> No Ticks Yet</h3>
-    }
-
-    return ( 
-        <section className='tick-list-container'>
-            <h3> Ticks </h3>
-            <div className='tick-list-header'>
-                <th className='route-name'> Route Name </th>
-                <th className='difficulty'> Grade </th>
-            </div>
-            
-            { user.ticks.map((tick, i) => {
-                return(
-                    <div className='tick-list' key={tick._id}> 
-                        <div className='route-name'>{tick.route_name} </div>
-                        <div className='difficulty'>{tick.difficulty} </div>
-                    </div>)
-                })
-            }
-        </section>
-    );
+    return (
+        !user.ticks.length ? (
+            <section className='tick-list-container'>
+                <h3> No Ticks Yet</h3>
+            </section>
+            ) : ( 
+            <section className='tick-list-container'>
+                <h3> Ticks </h3>
+                <div className='tick-list-header'>
+                    <th className='route-name'> Route Name </th>
+                    <th className='difficulty'> Grade </th>
+                </div>
+                
+                { user.ticks.map((tick, i) => {
+                    return(
+                        <div className='tick-list' key={tick._id}> 
+                            <div className='route-name'>{tick.route_name} </div>
+                            <div className='difficulty'>{tick.difficulty} </div>
+                        </div>)
+                    })
+                }
+            </section>
+        )
+    ); 
 }
  
 export default TickList;
