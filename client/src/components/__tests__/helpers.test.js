3dce311f71faf48c9d98382f4b83d56c2fa35efb
Sam Sweigart <samuel.sweigart@gmail.com>
2022-07-01 12:49:32 -0400

    added __test__ dir & quick first test

---
+++ client/src/components/__tests__/helpers.test.js
@@ -0,0 +1,22 @@
+import { capFirstChar } from '../../utils/helpers';
+
+
+
+// TODO: TEST BLOCK
+//* render component to test
+//* find elements to interact with
+//* interact with said elements
+//* assert that results are as expected
+
+
+
+test('Capitalize first character of string', () => {
+    let expectedVal = 'Test';
+    expect(capFirstChar('test')).toBe(expectedVal);
+
+});
+
+
+test('test', () => {
+    expect(true).toBe(true);
+});
